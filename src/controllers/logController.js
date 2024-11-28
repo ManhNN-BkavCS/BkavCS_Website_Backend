1a64ca032a961d125d95099a6e0ad448052b6382
huyluu200202 <125843555+huyluu200202@users.noreply.github.com>
2024-11-28 22:46:27 +0700

    Update logController.js

--- src/controllers/logController.js
+++ src/controllers/logController.js
@@ -3,12 +3,11 @@ const loginLogs = require('../models/login_logs')(sequelize);
 
 const getLogs = async (req, res) => {
     try {
-        // Fetch logs from the database
         const logs = await loginLogs.findAll();
-        res.status(200).json(logs);  // Return the logs as JSON
+        res.status(200).json(logs);  
     } catch (error) {
-        console.error(error);  // Log the error to the console
-        res.status(500).json({ message: 'Error fetching logs' });  // Handle errors
+        console.error(error);  
+        res.status(500).json({ message: 'Error fetching logs' });  
     }
 };
 
